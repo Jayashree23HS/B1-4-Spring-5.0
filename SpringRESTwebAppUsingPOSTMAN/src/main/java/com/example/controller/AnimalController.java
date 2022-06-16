0730df882d70156dc7c7bbd6b493392b1ec9619a
DELL <DELL@DESKTOP-00IENG9>
2022-06-16 22:19:00 +0530

    first commit

---
+++ SpringRESTwebAppUsingPOSTMAN/src/main/java/com/example/controller/AnimalController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Response;
+@RestController
+public class AnimalController 
+{
+	@GetMapping(path="/animal", produces="application/txt")
+	public ResponseEntity<Response>getAnimalDetails()
+	{
+		Response response=new Response();
+		response.setData("Lion");
+		response.setResponseCode(11);
+		response.setStatus("Success");
+		int x=10;
+		if(x==10)
+			throw new RuntimeException();
+		
+		return new ResponseEntity<>(response,HttpStatus.OK);
+	}
+}
